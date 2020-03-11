60d755f9b897bb47e60feb3f0dc64c50440528b0
Sapir Shemer <lesscomplexity@gmail.com>
2020-03-11 10:02:01 +0200

    Create table of item in create item function

--- machine/core/DataModelManager.php
+++ machine/core/DataModelManager.php
@@ -8,4 +8,23 @@
  */
 
 namespace APIShift\Core;
+
+use DatabaseManager;
+
+class DataModelManager {
+    public static function createItem(string $item_name, array $keys) {
+        // Check if table exists
+        $check = DatabaseManager::getInstance("main")->query("SELECT id FROM " . $item_name);
+        if(gettype($check) == 'array') {
+            // TODO: update existing structure
+            return;
+        }
+        
+        // TODO: create new table
+        // Create query string containing the key pairs and their data
+        $data = "";
+        foreach($keys as $key_name => $value) $data .= $key_name . " " . $value['type'] . ",";
+        $result = DatabaseManager::getInstance("main")->query("CREATE TABLE " . $item_name  .'{' . "id primary INT, " . $data . '}', $keys);
+    }
+}
 ?>

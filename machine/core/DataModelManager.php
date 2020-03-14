b16434afe99064e9c66b1077ac34fc3907f88560
LessComplexity <lesscomplexity@gmail.com>
2020-03-14 18:56:43 +0200

    Add function to create items

--- machine/core/DataModelManager.php
+++ machine/core/DataModelManager.php
@@ -9,22 +9,37 @@
 
 namespace APIShift\Core;
 
-use DatabaseManager;
-
 class DataModelManager {
-    public static function createItem(string $item_name, array $keys) {
+    /**
+     * Creates an item/relation using query rules
+     * Query structure:
+     * [
+     *      name => "<item/relation name>",
+     *      keys => [
+     *                  key_name_1: 'type',
+     *                  .
+     *                  .
+     *                  .
+     *              ],
+     *      relation_type: type_id \\ In case of a relation
+     *      from: <item_id/item_name/array>,
+     *      to: <item_id/item_name/array>
+     * ]
+     */
+    public static function create(string $query) {
+        // Validate query
+        if(!isset($query['name'])) Status::message(Status::ERROR, "No item/relation name specified");
+        if(!isset($query['keys'])) Status::message(Status::ERROR, "Cannot create an empty item/relation structure");
+
         // Check if table exists
-        $check = DatabaseManager::getInstance("main")->query("SELECT id FROM " . $item_name);
-        if(gettype($check) == 'array') {
-            // TODO: update existing structure
-            return;
-        }
+        $check = DatabaseManager::getInstance("main")->query("SELECT id FROM " . $query['name']);
+        if(gettype($check) == 'array') Status::message(Status::ERROR, "Item/relation exist");
         
-        // TODO: create new table
         // Create query string containing the key pairs and their data
-        $data = "";
-        foreach($keys as $key_name => $value) $data .= $key_name . " " . $value['type'] . ",";
-        $result = DatabaseManager::getInstance("main")->query("CREATE TABLE " . $item_name  .'{' . "id primary INT, " . $data . '}', $keys);
+        $query_str = "";
+        foreach($query['keys'] as $key_name => $value) $query_str .= $key_name . " " . $value . ",\n";
+        // Create the table
+        $result = DatabaseManager::getInstance("main")->query("CREATE TABLE " . $query['name']  . '{' . "id primary INT, " . $query_str . '}');
     }
 }
 ?>

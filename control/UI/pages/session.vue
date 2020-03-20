fa0f08fdb96594cdfd8f6c4dacc5cb2b0d99271f
LessComplexity <lesscomplexity@gmail.com>
2020-03-20 19:22:19 +0200

    View children button

--- control/UI/pages/session.vue
+++ control/UI/pages/session.vue
@@ -34,38 +34,23 @@
                                 <v-btn icon>
                                     <v-icon>mdi-minus-circle</v-icon>
                                 </v-btn>
+                                <v-btn icon>
+                                    <v-icon small>fas fa-lock</v-icon>
+                                </v-btn>
                             </v-card-title>
 
                             <v-card-text>
                                 <v-form>
-                                    <v-text-field type="text" lable="name" value="ADMIN_STATE" disabled></v-text-field>
-                                    <v-text-field type="number" lable="active timeout" disabled value="0"></v-text-field>
-                                    <v-text-field type="number" lable="inactive timeout" disabled value="0"></v-text-field>
+                                    <v-text-field class="session_field" type="text" lable="name" id="name" value="ADMIN_STATE" box disabled></v-text-field>
+                                    <v-text-field class="session_field" type="number" lable="active timeout" box disabled value="0"></v-text-field>
+                                    <v-text-field class="session_field" type="number" lable="inactive timeout" box disabled value="0"></v-text-field>
                                 </v-form>
                             </v-card-text>
 
-                            <v-card-actions bottom>
-                            </v-card-actions>
-                        </v-card>
-                    </v-hover>
-                    <v-hover v-slot:default="{ hover }">
-                        <v-card outlined class="px-0 session-card" :elevation="hover ? 16 : 2">
-                            <v-card-title>
-                                <v-icon left>
-                                    fas fa-user
-                                </v-icon>
-                                ADMIN_STATE
-                                <v-spacer></v-spacer>
-                                <v-btn icon>
-                                    <v-icon>mdi-pencil-box</v-icon>
-                                </v-btn>
-                                <v-btn icon>
-                                    <v-icon>mdi-minus-circle</v-icon>
-                                </v-btn>
-                            </v-card-title>
-
                             <v-card-actions>
-
+                                <v-btn text color="purple accent-4" width="100%">
+                                    View Children
+                                </v-btn>
                             </v-card-actions>
                         </v-card>
                     </v-hover>
@@ -84,6 +69,10 @@
 .session-card {
     margin: 5px;
     min-width: 300px;
+}
+
+.session_field {
+    padding-top: 0;
 }
 
 </style>

621b518f60a7d0f78ebba764d0984240c0c1e2d9
LessComplexity <lesscomplexity@gmail.com>
2020-03-21 21:13:12 +0200

    Add manipulation features to session state

--- control/UI/pages/session.vue
+++ control/UI/pages/session.vue
@@ -10,6 +10,7 @@
         data() {
             return {
                 states_collection: {},
+                editor_model: {},
                 current_parent: 0,
                 in_edit: 0,
                 delete_dialog: false,
@@ -21,26 +22,36 @@
             APIShift.API.request("SessionState", "getAllSessionStates", {}, function(response) {
                 if(response.status == true) {
                     handler.states_collection = response.data;
-                } else {
-                    APIShift.API.notify("Couldn't retrieve statuses", 'error');
+                }
+                else {
+                    APIShift.API.notify(response.data, 'error');
                 }
             }, true);
         },
         methods: {
-            startEdit: function(id) {
-                if(this.in_edit == 0) this.in_edit = id;
-                else {
-                    APIShift.API.notify("Edit in progress");
-                }
-            },
-            saveEdit: function() {
-                this.in_edit = 0;
-            },
-            deleteState: function() {
+            createState: function() {
 
             },
-            discardChanges: function() {
-                
+            saveState: function () {
+
+            },
+            startStateEdit: function(id) {
+                if(this.in_edit != 0) {
+                    APIShift.API.notify("Edit in progress");
+                }
+                this.in_edit = id;
+            },
+            saveStateEdit: function() {
+                this.in_edit = 0;
+            },
+            discardStateEdit: function() {
+
+                // Close dialog
+                this.discard_dialog = false;
+            },
+            deleteState: function(id) {
+                // Close dialog
+                this.delete_dialog = false;
             }
         },
     };
@@ -50,11 +61,12 @@
     <v-content>
         <v-container fluid fill-height>
             <v-card class="mx-auto" width="90%" min-height="75%" elevation="2">
+                <!-- Header -->
                 <v-app-bar>
                     <v-toolbar-title>Manage Sessions</v-toolbar-title>
                     <v-spacer></v-spacer>
                     <v-tooltip top>
-                        <template v-slot:activator="{ on }">
+                        <template #activator="{ on }">
                             <v-btn icon v-on="on">
                                 <v-icon>mdi-plus-circle</v-icon>
                             </v-btn>
@@ -62,38 +74,83 @@
                         <span>Add new session state</span>
                     </v-tooltip>
                 </v-app-bar>
+
+                <!-- Body -->
                 <div class="overflow-box" v-bar :class="$vuetify.theme.dark == true ? 'dark_bar' : 'light_bar'">
                     <div>
                         <v-layout class="mx-auto" align-start justify-center row wrap>
+                            <!-- Iterate through session states and show them -->
                             <v-hover v-for="(val, key) in states_collection" :key="key" v-slot:default="{ hover }" v-if="val.parent == current_parent">
                                 <v-card outlined class="px-0 session-card" :elevation="hover ? 16 : 2">
+                                    <!-- Session state header with name & actions -->
                                     <v-toolbar>
                                         <v-toolbar-title>{{ val.name }}</v-toolbar-title>
                                         <v-spacer></v-spacer>
+                                        <!-- Discard changes -->
+                                        <v-tooltip v-if="key == in_edit" top>
+                                            <template #activator="{ on }">
+                                                <v-btn icon v-on="on" @click="discard_dialog = true">
+                                                    <v-icon>mdi-close-circle</v-icon>
+                                                </v-btn>
+                                            </template>
+                                            <span>Discard Changes</span>
+                                        </v-tooltip>
+                                        <v-dialog v-model="discard_dialog" max-width="450px">
+                                            <v-card>
+                                                <v-card-title>Sure?</v-card-title>
+                                                <v-card-text>
+                                                    You cannot go back to changes you've discarded
+                                                </v-card-text>
+                                                <v-divider></v-divider>
+                                                <v-card-actions>
+                                                    <v-spacer></v-spacer>
+                                                    <v-btn color="primary" text @click="discardStateEdit()">Discard</v-btn>
+                                                    <v-btn text @click="discard_dialog = false">Cancel</v-btn>
+                                                </v-card-actions>
+                                            </v-card>
+                                        </v-dialog>
+                                        <!-- Edit state dialog & trigger -->
                                         <v-tooltip top>
-                                            <template v-slot:activator="{ on }">
-                                                <v-btn v-if="key == in_edit" icon v-on="on" @click="saveEdit()">
+                                            <template #activator="{ on }">
+                                                <v-btn v-if="key == in_edit" icon v-on="on" @click="saveStateEdit()">
                                                     <v-icon>mdi-checkbox-marked-circle</v-icon>
                                                 </v-btn>
-                                                <v-btn v-else icon v-on="on" @click="startEdit(key)">
+                                                <v-btn v-else icon v-on="on" @click="startStateEdit(key)" :disabled="in_edit != key && in_edit != 0">
                                                     <v-icon>mdi-pencil-circle</v-icon>
                                                 </v-btn>
                                             </template>
-                                            <span>Edit</span>
+                                            <span v-if="key == in_edit">Save Changes</span>
+                                            <span v-else>Edit</span>
                                         </v-tooltip>
+                                        <!-- Remove state dialog & trigger -->
                                         <v-tooltip top>
-                                            <template v-slot:activator="{ on }">
-                                            <v-btn icon v-on="on">
-                                                <v-icon>mdi-minus-circle</v-icon>
-                                            </v-btn>
+                                            <template #activator="{ on }">
+                                                <v-btn icon v-on="on" @click="delete_dialog = true">
+                                                    <v-icon>mdi-minus-circle</v-icon>
+                                                </v-btn>
                                             </template>
                                             <span>Remove</span>
                                         </v-tooltip>
+                                        <v-dialog v-model="delete_dialog" max-width="450px">
+                                            <v-card>
+                                                <v-card-title>Sure?</v-card-title>
+                                                <v-card-text>
+                                                    Deleting the session state will remove all authorization processes attached to it.
+                                                </v-card-text>
+                                                <v-divider></v-divider>
+                                                <v-card-actions>
+                                                    <v-spacer></v-spacer>
+                                                    <v-btn color="primary" text @click="deleteState(key)">Remove</v-btn>
+                                                    <v-btn text @click="delete_dialog = false">Cancel</v-btn>
+                                                </v-card-actions>
+                                            </v-card>
+                                        </v-dialog>
+                                        <!-- Authorization trigger -->
                                         <v-tooltip top>
-                                            <template v-slot:activator="{ on }">
-                                            <v-btn icon v-on="on" to="/access">
-                                                <v-icon small>fas fa-lock</v-icon>
-                                            </v-btn>
+                                            <template #activator="{ on }">
+                                                <v-btn icon v-on="on" to="/access">
+                                                    <v-icon small>fas fa-lock</v-icon>
+                                                </v-btn>
                                             </template>
                                             <span>Define Authorization Process</span>
                                         </v-tooltip>
@@ -102,7 +159,7 @@
                                     <v-card-text>
                                         <v-form>
                                             <v-tooltip top>
-                                                <template v-slot:activator="{ on }">
+                                                <template #activator="{ on }">
                                                     <v-text-field v-on="on"
                                                         class="session_field"
                                                         type="text" label="name"
@@ -112,7 +169,7 @@
                                                 <span>Session Name</span>
                                             </v-tooltip>
                                             <v-tooltip top>
-                                                <template v-slot:activator="{ on }">
+                                                <template #activator="{ on }">
                                                     <v-text-field v-on="on"
                                                         class="session_field"
                                                         type="number"
@@ -123,7 +180,7 @@
                                                 <span>Timeout When User Active (s)</span>
                                             </v-tooltip>
                                             <v-tooltip top>
-                                                <template v-slot:activator="{ on }">
+                                                <template #activator="{ on }">
                                                     <v-text-field v-on="on"
                                                         class="session_field"
                                                         type="number"

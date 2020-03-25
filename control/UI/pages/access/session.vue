1f7a0c5d32dd80e20d30a612bcbc644155eb9f10
LessComplexity <lesscomplexity@gmail.com>
2020-03-25 20:25:13 +0200

    Access pages template

---
+++ control/UI/pages/access/session.vue
@@ -0,0 +1,20 @@
+<script>
+    /**
+     * APIShift Engine v1.0.0
+     * (c) 2020-present Sapir Shemer, DevShift (devshift.biz)
+     * Released under the MIT License with the additions present in the LICENSE.md
+     * file in the root folder of the APIShift Engine original release source-code
+     * @author Sapir Shemer
+     */
+</script>
+
+<template>
+    <v-content>
+        <v-container fluid fill-height>
+            <h1>Comming Soon! :)</h1>
+        </v-container>
+    </v-content>
+</template>
+<style scoped>
+
+</style>

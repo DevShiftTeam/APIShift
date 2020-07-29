51888c6400ff46775e3ca85c46538848e22620a1
LessComplexity <lesscomplexity@gmail.com>
2020-07-29 15:00:33 +0300

    Fix DB COLLATE to fit cross-database

--- machine/data/initial.sql
+++ machine/data/initial.sql
@@ -247,7 +247,7 @@ CREATE TABLE `input_values` (
   `is_source` tinyint(1) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`,`value`,`is_source`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -271,7 +271,7 @@ CREATE TABLE `inputs` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
